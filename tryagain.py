18fd4f21bbdc23c84f691c0aee593e6bf4c14ade
ganesh8074 <ganeshchowdary657@gmail.com>
2021-04-12 01:57:33 +0530

    first commiit

---
+++ tryagain.py
@@ -0,0 +1,9 @@
+while True:
+    try:
+        a = int(input("Input a number: "))
+        break
+    except ValueError:
+        print("\nThis is not a number. Try again...")
+          print("Eat something you want")
+        print("eat apple")
+        print()
